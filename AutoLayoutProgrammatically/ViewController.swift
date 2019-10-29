94ca965f42c593cfe1daeafab8cde7f253cf8a96
Frank Su <candyline25@gmail.com>
2019-10-29 15:30:07 -0700

    Initial project AutoLayoutProgrammatically

---
+++ AutoLayoutProgrammatically/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AutoLayoutProgrammatically
+//
+//  Created by Frank Su on 2019-10-25.
+//  Copyright © 2019 frankusu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow 
+    }
+
+
+}
+
